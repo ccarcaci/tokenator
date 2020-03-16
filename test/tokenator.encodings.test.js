2bc1dcbc922492f24c51dac1aec335cbbb105010
Claudio Carcaci <claudiocarcaci@gmail.com>
2020-03-16 19:32:59 +0100

    Remove tokenator encoding tests

--- test/tokenator.encodings.test.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-const tokenator = require("../src/tokenator")
-
-describe("Run against extended ascii string", () => {
-  test("ÇüÇ", () => expect(tokenator("ÇüÇ", "ü")).toEqual(["Ç", "ü", "Ç"]))
-})
